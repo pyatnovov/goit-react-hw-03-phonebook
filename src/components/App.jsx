7dc975b9549a1359e35edb0fe86b5cd8994c8b83
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-02-27 21:42:56 +0200

    Готова робота

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,20 @@ export class App extends Component {
     ],
     filter: '',
   };
-
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+      return;
+    }
+    this.setState({ contacts: this.state.contacts });
+  }
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   handleChange = e => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
